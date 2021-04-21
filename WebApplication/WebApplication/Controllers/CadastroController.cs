77fa5d7f79f40350b78a15bab687007dd09d0cd6
THIAGO ARAUJO <thiago.araujo89@etec.sp.gov.br>
2021-04-21 12:21:21 -0300

    Criação da primeira interface da aplicação seguindo os padrões MVC. Até esse momento todos os dados já estão sendo obtidos e retornados ao usuário.

---
+++ WebApplication/WebApplication/Controllers/CadastroController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult cadastrocarro([FromForm] Carro carro)
+        {
+            if (ModelState.IsValid)
+            {
+                string conteudo = string.Format("Fabricaante: {0}, " +
+                                 "Modelo: {1}, " +
+                                 "Vesão: {2}, " +
+                                 "Ano: {3}, " +
+                                 "Cor: {4}. ",
+                                 carro.fabricante, carro.modelo, carro.versao, carro.ano, carro.cor);
+
+                return new ContentResult() { Content = conteudo };
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Models/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Carro
+    {
+        public string fabricante { get; set; }
+        public string modelo { get; set; }        
+        public string versao { get; set; }
+        public int ano { get; set; }
+        public string cor { get; set; }
+    }
+}
