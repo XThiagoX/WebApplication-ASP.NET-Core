7c16878f26bd6ab81cf534b3225760232d84a687
THIAGO ARAUJO <thiago.araujo89@etec.sp.gov.br>
2021-04-21 18:47:17 -0300

    Validações de entrada realizadas(na class Carro.cs)

--- WebApplication/WebApplication/Models/Carro.cs
+++ WebApplication/WebApplication/Models/Carro.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication.Models
 {
     public class Carro
     {
+        [Required (ErrorMessage="Campo Obrigatório!")]
         public string fabricante { get; set; }
-        public string modelo { get; set; }        
+
+        [Required (ErrorMessage="Campo Obrigatório!")]
+        public string modelo { get; set; }
+                
         public string versao { get; set; }
+        
+        [Required (ErrorMessage="Campo Obrigatório!")]
         public int ano { get; set; }
+
         public string cor { get; set; }
     }
 }
